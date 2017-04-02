e0ba0b36da1010553fe5d579b42d3f3053b35a71
PeaceOff <davidazevedo1996@hotmail.com>
2017-04-02 22:49:34 +0100

    Tested AST

--- src/compile.bat
+++ src/compile.bat
@@ -1,2 +1,2 @@
-echo JJTREE: & jjtree Simple2.jjt && echo JAVACC: & javacc Simple2.jj &&  echo JAVAC: & javac -d ./output *.java
+echo JJTREE: & jjtree Simple2.jjt && echo JAVACC: & javacc Simple2.jj &&  echo JAVAC: & javac -d ../output *.java
 pause
